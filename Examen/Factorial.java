e73132b8a99d3315ff2dbfc83aadc7a949826321
Alejandra Soledad Ramírez Pastor <alejandrasoledadrmirezpastor@gmail.com>
2025-10-02 18:01:04 -0600

    Create Factorial.java

---
+++ Examen/Factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int factorial(int numero) {
+        if (numero == 0)
+            return 1;
+        else
+            return numero * factorial(numero-1);
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Factorial de un numero");
+        System.out.print("Introduce un numero: ");
+        int numero = teclado.nextInt();
+        teclado.close();
+        System.out.println("El factorial de " + numero + "!" + " es: " + factorial(numero));
+    }
+    
+}
